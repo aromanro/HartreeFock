e8c6d4939c10b956f37f074aec676c9c4866cb9b
Adrian Roman <aroman_ro@yahoo.com>
2019-09-07 13:14:22 +0300

    Switching to VisualStudio 2019

--- HartreeFock/Atom.h
+++ HartreeFock/Atom.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 
 #include "Vector3D.h"
 #include "PrimitiveShell.h"
--- HartreeFock/PrimitiveShell.h
+++ HartreeFock/PrimitiveShell.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 #include <cassert>
 
 #include "GaussianOrbital.h"
